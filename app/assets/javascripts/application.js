155082114902b2b6029b3878170b07b82f7f9569
adeebaah <adebaislam@iut-dhaka.edu>
2024-06-28 21:40:49 +0600

    added about page and navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,21 @@
-$('#button').toggle(
-    function() {
-        $('#right').animate({ left: 250 }, 'slow', function() {
-            $('#button').html('Close');
-        });
-    },
-    function() {
-        $('#right').animate({ left: 0 }, 'slow', function() {
-            $('#button').html('Menu');
-        });
-    }
-);
+// app/assets/javascripts/application.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    var checkBox = document.getElementById('check');
+    var btnIcon = document.getElementById('btn');
+    var cancelIcon = document.getElementById('cancel');
+
+    checkBox.addEventListener('change', function() {
+        if (checkBox.checked) {
+            btnIcon.style.opacity = '0';
+            btnIcon.style.visibility = 'hidden';
+            cancelIcon.style.opacity = '1';
+            cancelIcon.style.visibility = 'visible';
+        } else {
+            btnIcon.style.opacity = '1';
+            btnIcon.style.visibility = 'visible';
+            cancelIcon.style.opacity = '0';
+            cancelIcon.style.visibility = 'hidden';
+        }
+    });
+});
